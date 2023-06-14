98ea19251692c187c49ee318e7ecb8932465e4e1
Nicolas Mazo Montoya <nicolasmazo88@gmail.com>
2023-06-14 09:18:31 -0500

    Guardar cambios locales

--- src/controllers/validateToken.js
+++ src/controllers/validateToken.js
@@ -1,7 +1,6 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-  console.log("entra");
   const token = req.headers["authorization"];
 
   if (!token) {
@@ -15,10 +14,9 @@ export const verifyToken = (req, res, next) => {
       return res.status(401).json({
         message: "El token de autenticación es inválido",
       });
+    } else {
+      req.userId = { id: decoded.email };
+      next();
     }
-    console.log(decoded);
-
-    req.userId = { id: decoded.email };
-    next();
   });
 };
